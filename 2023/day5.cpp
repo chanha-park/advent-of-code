13e5d7b0af447cf255842f7b09b3d5c0d29dc105
chanha-park <chanha.dev@gmail.com>
2023-12-07 22:23:43 +0900

    2023-day5-1.cpp

--- 2023/day5.cpp
+++ 2023/day5.cpp
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
+#include <map>
+#include <optional>
 #include <sstream>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 enum {
@@ -18,34 +20,56 @@ enum {
 
 int
 main(void) {
-    std::vector<int> seeds;
-    std::unordered_map<int, int> maps[NUMBER_OF_MAPS];
+    std::vector<long long> seeds;
+    std::map<long long, long long> maps[NUMBER_OF_MAPS];
 
     {
-        char buff[200];
+        char buff[1000];
         scanf("seeds: %[^\n]", buff);
         std::stringstream ss(buff);
         while (!ss.eof()) {
-            int n;
+            long long n;
             ss >> n;
             seeds.push_back(n);
         }
     }
-    std::cout << std::endl;
+
     {
         std::string line;
         std::getline(std::cin, line);
     }
+
     for (int i = 0; i < NUMBER_OF_MAPS; ++i) {
         std::string line;
         std::getline(std::cin, line);
         while (std::getline(std::cin, line) && line != "") {
             std::stringstream ss(line);
-            int dst, src, range;
+            long long dst, src, range;
             ss >> dst >> src >> range;
             maps[i][src] = dst - src;
             maps[i][src + range];  // XXX
         }
     }
+
+    long long min_location = -1;
+
+    auto const& convert = [&maps, &min_location](long long curr) {
+        for (int i = 0; i < NUMBER_OF_MAPS; ++i) {
+            std::map<long long, long long> const& map = maps[i];
+            auto it = map.upper_bound(curr);
+            if (it != map.cbegin()) {
+                --it;
+                curr += it->second;
+            }
+        }
+        min_location
+            = (min_location > curr || min_location == -1) ? curr : min_location;
+    };
+
+    for (auto const seed : seeds) {
+        convert(seed);
+    }
+    std::cout << min_location << std::endl;
+
     return 0;
 }
